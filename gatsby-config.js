222217a013a84a72753255ac70bdeaaa9cd558a1
ricksubel <ricksubel@gmail.com>
2021-05-24 18:20:40 -0400

    tries to address accessToken and spaceId issue in build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,16 @@
+let activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log('Using environment config: ${activeEnv}')
 
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
-  flags: {
-    DEV_SSR: false,
-  },
+  // flags: {
+  //   DEV_SSR: false,
+  // },
   siteMetadata: {
     title: `a static site...`,
     author: `rick subel`,
@@ -27,6 +31,12 @@ module.exports = {
         name: `src`,
         path: `${__dirname}/src/`,
       },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+    },
+    {
+      resolve: `gatsby-plugin-image`,
     },
     {
       resolve: `gatsby-plugin-emotion`,
